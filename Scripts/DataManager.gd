1047d3f95f83613389e3cc9a00cc15ae878c4a4c
MathewHDYT <mathe@mathew.gmuer@gmail.com>
2021-10-08 23:04:59 +0200

    Adjusting project to save file data between game sessions and added UI to test the functionality of the DataManager

--- Scripts/DataManager.gd
+++ Scripts/DataManager.gd
@@ -8,145 +8,185 @@ extends Node
 # encryption (bool): Wether the given file should be encrypted.
 # hashing (bool): Wheter the given file should be checked for unexpected changes before using it.
 # compression (int or File.CompressionMode): Wheter and with which compression algorithm the given file should be compressed.
-func create_new_file(file_name : String, content := "", directory_path := "user://", file_ending := ".txt", encryption := false, hashing := false, compression := -1) -> bool:
-    var success := false
+func create_new_file(file_name : String, content := "", directory_path := "user://", file_ending := ".txt", encryption := false, hashing := false, compression := -1) -> int:
+    var err := OK
     # Check if the directory path ends with a delimeter if it doesn't add one.
     if directory_path[-1] != '/':
         directory_path += '/'
     # Get the file_path from the given values and save it into a FileData object.
     var file_path := directory_path + file_name + file_ending
+    var dir := Directory.new()
+    if (!dir.dir_exists(directory_path)):
+        err = ERR_FILE_BAD_PATH
+        return err
     var file_hash := ""
     # Add data of the newly created file to the dictionary.
     var file_data := FileData.new(file_path, file_hash, encryption, compression);
     _add_to_dictionary(file_name, file_data)
-    if (_check_file(file_path)):
-        return success
+    err = _check_file(file_path)
+    if (err != OK):
+        return err
     if (encryption && compression >= 0):
-        printerr("File can't be both encrypted and compressed")
-        return success
-    success = _detect_write_mode(file_data, content, File.WRITE)
+        err = ERR_INVALID_PARAMETER
+        return err
+    err = _detect_write_mode(file_data, content, File.WRITE)
     # Check if the file should be hashed.
     if (hashing):
         file_data.set_file_hash(_get_file_hash(file_path))
-    return success
+    return err
 
 # Returns the text from the given file. Returns an empty string,
 # when the file is nto yet registered or when the hash is not as expected,
 # if hashing is enabled for the given file.
 # file_name (String): Name of the given file that should be read from.
-func read_from_file(file_name : String) -> String:
+func read_from_file(file_name : String) -> Array:
+    var err := OK
     var content = ""
-    var file_data := _get_file_data(file_name)
-    if (file_data == null):
-        return content
+    var result := _get_file_data(file_name)
+    var file_data = result[0]
+    err = result[1]
+    if (err != OK):
+        return [content, err]
     # Check if hashing is enabled and we therefore saved the latest hash in file_data.
     # Check if the hash is still the same or if it was changed.
     if (file_data.get_file_hash() != "" && !compare_hash(file_name)):
         # If it was don't read our given content from the file.
-        return content
+        err = ERR_FILE_CORRUPT
+        return [content, err]
     # Check if the file should be only encrypted.
-    if (file_data.get_file_encryption()):
-        content = _read_from_encrypted_file(file_data.get_file_path(), File.READ)
+    if (file_data.get_file_key()):
+        result = _read_from_encrypted_file(file_data.get_file_path(), File.READ)
+        content = result[0]
+        err = result[1]
     # Check if the file should be only compressed.
     elif (file_data.get_file_compression() >= 0):
-        content = _read_from_compressed_file(file_data, File.READ)
+        result = _read_from_compressed_file(file_data, File.READ)
+        content = result[0]
+        err = result[1]
     else:
-        content = _read_from_file(file_data.get_file_path(), File.READ)
-    return content
+        result = _read_from_file(file_data.get_file_path(), File.READ)
+        content = result[0]
+        err = result[1]
+    return [content, err]
 
 # Changes the file location of the given file to the new directory and
 # returns true if it succeeded.
 # file_name (String): Name of the given file that should be moved.
-# directory (String): New directory the given file should be moved too.
-func change_file_path(file_name : String, directory : String) -> bool:
-    var success := false
-    var file_data := _get_file_data(file_name)
-    if (file_data == null):
-        return success
+# directory_path (String): New directory the given file should be moved too.
+func change_file_path(file_name : String, directory_path : String) -> int:
+    var err := OK
+    # Check if the directory path ends with a delimeter if it doesn't add one.
+    if directory_path[-1] != '/':
+        directory_path += '/'
+    var result := _get_file_data(file_name)
+    var file_data = result[0]
+    err = result[1]
+    if (err != OK):
+        return err
     # Check if the given path exists.
     var dir := Directory.new()
-    if (!dir.dir_exists(directory)):
-        printerr("Given directory: " + directory + " does not exist");
-        return success
+    if (!dir.dir_exists(directory_path)):
+        err = ERR_FILE_BAD_PATH
+        return err
     # Split the full file_path into the seperate directories, subdirectories and the filename with extnesion,
     # then get the last element of that array, which is the filename with extension.
-    var name := file_data.get_file_path().split("/")[-1]
-    var file_path := directory + name
+    var name = file_data.get_file_path().split("/")[-1]
+    var file_path = directory_path + name
     # Check if the file exists already at the given path.
-    if (_check_file(file_path)):
-        return success
+    err = _check_file(file_path)
+    if (err != OK):
+        return err
     # Move the file to its new location and adjust the file_path to the new value.
-    var err := dir.rename(file_data.get_file_path(), file_path)
-    success = err == OK
+    err = dir.rename(file_data.get_file_path(), file_path)
+    if (err != OK):
+        return err
     file_data.set_file_path(file_path)
-    return success
+    return err
 
 # Updates the content of the given file, completly replacing the current content and
 # returning true if succesful and false if not.
 # file_name (String): Name of the given file that should have its content replaced.
 # content (String): Data that should be saved into the file.
-func update_file_content(file_name : String, content : String) -> bool:
-    var success := false
-    var file_data := _get_file_data(file_name)
-    if (file_data == null):
-        return success
-    success = _detect_write_mode(file_data, content, File.WRITE)
-    return success
+func update_file_content(file_name : String, content : String) -> int:
+    var err := OK
+    var result := _get_file_data(file_name)
+    var file_data = result[0]
+    err = result[1]
+    if (err != OK):
+        return err
+    err = _detect_write_mode(file_data, content, File.WRITE)
+    return err
 
 # Appends the content to the given file, keeping the current content and
 # returning true if succesful and false if not. Will not be executed and return false,
 # if hashing is enabled and the hash of the file has changed still we last changed it ourselves.
 # file_name (String): Name of the given file that should have the content appended.
 # content (String): Data that should be appended to the file.
-func append_file_content(file_name : String, content : String) -> bool:
-    var success := false
-    var file_data := _get_file_data(file_name)
-    if (file_data == null):
-        return success
+func append_file_content(file_name : String, content : String) -> int:
+    var err := OK
+    var result := _get_file_data(file_name)
+    var file_data = result[0]
+    err = result[1]
+    if (err != OK):
+        return err
     # Check if hashing is enabled and we therefore saved the latest hash in file_data.
     # Check if the hash is still the same or if it was changed.
     if (file_data.get_file_hash() != "" && !compare_hash(file_name)):
         # If it was don't append our given content to the file.
-        return success
-    success = _detect_write_mode(file_data, content, File.READ_WRITE)
-    return success
+        err = ERR_FILE_CORRUPT
+        return err
+    err = _detect_write_mode(file_data, content, File.READ_WRITE)
+    return err
 
 # Compares the current hash with the last expected hash
 # and returns true if it is the same and false if it isn't.
 # file_name (String): Name of the given file that should have its hash checked.
-func check_file_hash(file_name : String) -> bool:
-    var result := false
-    var file_data := _get_file_data(file_name)
-    if (file_data == null):
-        return result
-	elif (file_data.get_file_hash() == ""):
-		return result
+func compare_hash(file_name : String) -> Array:
+    var err := OK
+    var success := false
+    var result := _get_file_data(file_name)
+    var file_data = result[0]
+    err = result[1]
+    if (err != OK):
+        return [success, err]
+    elif (file_data.get_file_hash() == ""):
+        err = ERR_FILE_MISSING_DEPENDENCIES
+        return [success, err]
     var current_hash := _get_file_hash(file_data.get_file_path())
-    result = current_hash == file_data.get_file_hash()
-    return result
+    success = current_hash == file_data.get_file_hash()
+    return [success, err]
 
 # Deletes the given file and unregisters it.
 # file_name (String): Name of the given file that should be deleted.
-func delete_file(file_name : String) -> bool:
+func delete_file(file_name : String) -> Array:
+    var err := OK
     var success := false
-    var file_data := _get_file_data(file_name)
-    if (file_data == null):
-        return success
-    elif (!_check_file(file_data.get_file_path(), true)):
-        return success
+    var result := _get_file_data(file_name)
+    var file_data = result[0]
+    err = result[1]
+    if (err != OK):
+        return [success, err]
+    err = _check_file(file_data.get_file_path(), true)
+    if (err != OK):
+        return [success, err]
     var dir := Directory.new()
-    var err := dir.remove(file_data.get_file_path())
-    if err != OK:
-        return success
+    err = dir.remove(file_data.get_file_path())
+    if (err != OK):
+        return [success, err]
+    err = dir.remove(FILEDATA_DICTIONARY + file_name + FILEDATA_FILE)
+    if (err != OK):
+        return [success, err]
     success = _remove_from_dictionary(file_name)
     _update_file_names()
-    return success
+    return [success, err]
 
 #-----------------------------------------------------------------------------
 # Private
 #-----------------------------------------------------------------------------
 
-const FILENAME_FILES := "user://file_name.save"
+const DICTIONARY_KEY_FILE := "user://file_name.save"
+const FILEDATA_DICTIONARY := "user://"
+const FILEDATA_FILE := "_file_data.save"
 const FILE_KEY := "ja9tEHvXzvJcwDOiwQoI"
 
 var _file_dictionary  := Dictionary()
@@ -200,7 +240,7 @@ class FileData:
 # Called when the game is started.
 func _ready() -> void:
     # Check if there has been a previous game session, with saved game files.
-    if (!_check_file(FILENAME_FILES, true)):
+    if (!_check_file(DICTIONARY_KEY_FILE, true)):
         # If there wasn't we need to create the file initially.
         _update_file_names()
     else:
@@ -208,37 +248,28 @@ func _ready() -> void:
         # into our dictionary.
         _load_file_names()
 
-# Checks if the file at the given location exists and returns a message,
-# if the expected result is different then the actual result.
-func _check_file(file_path : String, file_exists := false) -> bool:
-    var result := _get_file_state(file_path, file_exists)
-    var message = result[0]
-    var success = result[1]
-    if (message != ""):
-        printerr(message)
-    return success;
-
 # Gets the FileData object from the given file name otu of our dictionary.
-func _get_file_data(file_name : String) -> FileData:
+func _get_file_data(file_name : String) -> Array:
+    var err := OK
     var file_data = _file_dictionary.get(file_name)
     # Check if the key existed and we succesfully got the value.
-    if (file_data == null):
-        printerr("Given file name: " + file_name + " is not registered")
-    return file_data
+    if (file_data == null || !file_data is FileData):
+        err = ERR_FILE_UNRECOGNIZED
+    return [file_data, err]
 
-# Gets the current state of our file consisting of it it exists and the error message,
-# dpeednign on if the result is different then the expected result.
-func _get_file_state(file_path : String, file_exists : bool) -> Array:
+# Checks if the file at the given location exists and returns an error,
+# if the expected result is different then the actual result.
+func _check_file(file_path : String, file_exists := false) -> int:
+    var err := OK
     var file := File.new()
-    var result = file.file_exists(file_path)
+    var result := file.file_exists(file_path)
     file.close()
-    var message := ""
     if (result != file_exists):
         if (result):
-            message = "There already exists a file at the given path: " + file_path
+            err = ERR_ALREADY_EXISTS
         else:
-            message = "There doesn't exist a file at the given folder: " + file_path
-    return [message, result]
+            err = ERR_DOES_NOT_EXIST
+    return err
 
 # Add the given FileData to the dictionary, where the value is the FileData and the key is our file name
 func _add_to_dictionary(file_name : String, file_data : FileData) -> void:
@@ -256,26 +287,59 @@ func _remove_from_dictionary(file_name : String) -> bool:
 # Updates the save file that contains our dictionary data.
 func _update_file_names() -> void:
     var file = File.new()
-    file.open(FILENAME_FILES, File.WRITE)
-    # Convert the whole dictionary to a json string
-    var json := JSON.print(_file_dictionary)
+    file.open(DICTIONARY_KEY_FILE, File.WRITE)
+    # Convert the whole dictionary to a json string.
+    var json := JSON.print(_file_dictionary.keys())
     # and store it into our save file.
     file.store_string(json)
     file.close()
+    # Update or save the file data file for each file_data instance.
+    for key in _file_dictionary:
+        var save_path := FILEDATA_DICTIONARY + (key as String) + FILEDATA_FILE
+        file.open(save_path, File.WRITE)
+        # Get the needed data from the file data object instance.
+        var file_data := _file_dictionary[key] as FileData
+        var file_path := file_data.get_file_path()
+        var file_hash := file_data.get_file_hash()
+        var file_key := file_data.get_file_key()
+        var file_compression := file_data.get_file_compression()
+        # Create an array from that data and serialize it as a json.
+        var data_array = [file_path, file_hash, file_key, file_compression]
+        json = JSON.print(data_array)
+        file.store_string(json)
+        file.close()
 
 # Loads the save file dave and saves it into the dictionary.
 # This is needed to reconstruct the dictionary on game start,
 # so that the files are still accesible even if we restart the game.
 func _load_file_names() -> void:
     var file = File.new()
-    file.open(FILENAME_FILES, File.READ)
+    file.open(DICTIONARY_KEY_FILE, File.READ)
     var json = file.get_as_text()
     file.close()
     var parse_result := JSON.parse(json)
     if parse_result.error != OK:
         printerr(parse_result.error_string)
         return
-    _file_dictionary = parse_result.result
+    var dict_keys = parse_result.result
+    # Load the file_data file for each file_data instance.
+    for key in dict_keys:
+        var save_path := FILEDATA_DICTIONARY + (key as String) + FILEDATA_FILE
+        file.open(save_path, File.READ)
+        json = file.get_as_text()
+        file.close()
+        parse_result = JSON.parse(json)
+        if parse_result.error != OK:
+            printerr(parse_result.error_string)
+            return
+        var data_array = parse_result.result
+        # Get the needed data to create the file data object instance, from the data array object.
+        var file_path = data_array[0]
+        var file_hash = data_array[1]
+        var file_key = data_array[2]
+        var file_compression = data_array[3]
+        var file_data := FileData.new(file_path, file_hash, file_key, file_compression)
+        _add_to_dictionary(key, file_data)
 
 # Gets the file hash from the path of the given file.
 func _get_file_hash(file_path : String) -> String:
@@ -285,8 +349,8 @@ func _get_file_hash(file_path : String) -> String:
     return file_hash
 
 # Detects in which mode the file should be edited in.
-func _detect_write_mode(file_data : FileData, content : String, file_mode : int) -> bool:
-    var err := 0
+func _detect_write_mode(file_data : FileData, content : String, file_mode : int) -> int:
+    var err := OK
     # Check if encryption is enabled.
     if (file_data.get_file_key()):
         err = _write_to_encrypted_file(content, file_data.get_file_path(), file_mode)
@@ -297,7 +361,7 @@ func _detect_write_mode(file_data : FileData, content : String, file_mode : int)
     # Check if hashing is enabled and we therefore saved the latest hash in file_data.
     if (file_data.get_file_hash() != ""):
         file_data.set_file_hash(_get_file_hash(file_data.get_file_path()))
-    return err == OK
+    return err
 
 # Simply writes the given content to the given file.
 func _write_to_file(content : String, file_path : String, file_mode : int) -> int:
@@ -342,34 +406,37 @@ func _write_to_encrypted_file(content : String, file_path : String, file_mode : 
     return err
 
 # Simply reads the content from the given file.
-func _read_from_file(file_path : String, file_mode : int) -> String:
-    var file = File.new()
-    var err = file.open(file_path, file_mode)
+func _read_from_file(file_path : String, file_mode : int) -> Array:
+    var err := OK
     var content = ""
+    var file = File.new()
+    err = file.open(file_path, file_mode)
     if (err != OK):
-        return content
+        return [content, err]
     content = file.get_as_text()
     file.close()
-    return content
+    return [content, err]
 
 # Reads the content from the given compressed file.
-func _read_from_compressed_file(file_data : FileData, file_mode : int) -> String:
-    var file = File.new()
-    var err = file.open_compressed(file_data.get_file_path(), file_mode, file_data.get_file_compression())
+func _read_from_compressed_file(file_data : FileData, file_mode : int) -> Array:
+    var err := OK
     var content = ""
+    var file = File.new()
+    err = file.open_compressed(file_data.get_file_path(), file_mode, file_data.get_file_compression())
     if (err != OK):
-        return content
+        return [content, err]
     content = file.get_as_text()
     file.close()
-    return content
+    return [content, err]
 
 # Reads the content from the given encrypted file.
-func _read_from_encrypted_file(file_path : String, file_mode : int) -> String:
-    var file = File.new()
-    var err = file.open_encrypted_with_pass(file_path, file_mode, FILE_KEY)
+func _read_from_encrypted_file(file_path : String, file_mode : int) -> Array:
+    var err := OK
     var content = ""
+    var file = File.new()
+    err = file.open_encrypted_with_pass(file_path, file_mode, FILE_KEY)
     if (err != OK):
-        return content
+        return [content, err]
     content = file.get_as_text()
     file.close()
-    return content
+    return [content, err]
--- Scripts/MethodCall.gd
+++ Scripts/MethodCall.gd
@@ -1,9 +1,11 @@
 extends Node
 
-onready var _compression_mode := $CompressionModeInput/OptionButton
-onready var _encryption := $EncryptionInput/CheckButton
-onready var _file_name := $FileNameInput/TextEdit
-onready var _file_content := $FileContentInput/TextEdit
+onready var _compression_mode := $Input/CompressionModeInput/OptionButton
+onready var _encryption := $Input/EncryptionInput/CheckButton
+onready var _file_name := $Input/FileNameInput/TextEdit
+onready var _file_content := $Input/FileContentInput/TextEdit
+onready var _file_path := $Input/FilePathInput/TextEdit
+onready var _output := $Output/TextEdit
 
 func _ready() -> void:
     _compression_mode.add_item("None", 4)
@@ -12,13 +14,48 @@ func _ready() -> void:
     _compression_mode.add_item("Zstandard", File.COMPRESSION_ZSTD)
     _compression_mode.add_item("Gzip", File.COMPRESSION_GZIP)
 
-func _on_Button_pressed() -> void:
+func _on_CreateFile_pressed():
     var splits = _file_name.text.split(".")
     var file_name = splits[0]
     var file_extension = "." + splits[-1]
-    var content = _file_content.text
     var compression = _compression_mode.get_selected_id()
     var encryption = _encryption.is_pressed()
     if (compression == 4):
         compression = -1
-    DataManager.create_new_file(file_name, content, "C:/Users/Deutsch/Downloads", file_extension, encryption, false, compression)
+    if (!DataManager.create_new_file(file_name, _file_content.text, _file_path.text, file_extension, encryption, false, compression)):
+        _output.text = "Creating file failed"
+
+func _on_ReadFile_pressed():
+    var splits = _file_name.text.split(".")
+    var file_name = splits[0]
+    _file_content.text = DataManager.read_from_file(file_name)
+
+func _on_DeleteFile_pressed():
+    var splits = _file_name.text.split(".")
+    var file_name = splits[0]
+    if (!DataManager.delete_file(file_name)):
+        _output.text = "Deleting file failed"
+
+func _on_ChangeFilePath_pressed():
+    var splits = _file_name.text.split(".")
+    var file_name = splits[0]
+    if (!DataManager.change_file_path(file_name, _file_path.text)):
+        _output.text = "Changin file path failed"
+
+func _on_UpdateFile_pressed():
+    var splits = _file_name.text.split(".")
+    var file_name = splits[0]
+    if (!DataManager.update_file_content(file_name, _file_content.text)):
+        _output.text = "Updating file content failed"
+
+func _on_AppendFile_pressed():
+    var splits = _file_name.text.split(".")
+    var file_name = splits[0]
+    if (!DataManager.append_file_content(file_name, _file_content.text)):
+        _output.text = "Appending file content failed"
+
+func _on_CompareHash_pressed():
+    var splits = _file_name.text.split(".")
+    var file_name = splits[0]
+    if (!DataManager.compare_hash(file_name)):
+        _output.text = "Hash is different than expected or hashing is not enabled"
